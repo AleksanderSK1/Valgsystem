daa35191cb26aaee04128d2ce8c196986a483c1a
AleksanderSK1 <alkna009@osloskolen.no>
2025-09-10 15:18:19 +0200

    Create Valgsystem.py

---
+++ Valgsystem.py
@@ -0,0 +1,14 @@
+#Valgsystem
+
+
+
+Partier = ["Arbeiderpartiet", "Høyre", "Senterpartiet","Fremskrittspartiet", "Sosialistisk Venstreparti,", "Rødt", "Venstre", "Venstre", "Miljøpartiet de grønne", "Kristelig Folkeparti"]
+
+
+for i in range(len(Partier)):
+    print(Partier)
+    print("Velg et parti")
+    svar = input("Svar: ").upper
+    
+    
+    print("Du stemte på",svar)          
