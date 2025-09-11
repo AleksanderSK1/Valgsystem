4845704f9a6d0363e772157c88d23ea4ca919074
AleksanderSK1 <alkna009@osloskolen.no>
2025-09-11 20:01:08 +0200

    Update Valgsystem.py

--- Valgsystem.py
+++ Valgsystem.py
@@ -1,14 +1,21 @@
 #Valgsystem
 
 
+Partier = ["arbeiderpartiet", "høyre", "senterpartiet","fremskrittspartiet", "sosialistisk Venstreparti", "rødt", "venstre", "miljøpartiet de grønne", "kristelig Folkeparti"]
+Stemmer = [0] * len(Partier)
 
-Partier = ["Arbeiderpartiet", "Høyre", "Senterpartiet","Fremskrittspartiet", "Sosialistisk Venstreparti,", "Rødt", "Venstre", "Venstre", "Miljøpartiet de grønne", "Kristelig Folkeparti"]
+print("Partier:", Partier)
 
-
-for i in range(len(Partier)):
-    print(Partier)
+svar = ""
+while svar != "ferdig":
     print("Velg et parti")
-    svar = input("Svar: ").upper
-    
-    
-    print("Du stemte på",svar)          
+    svar = input("Svar: ")
+    if svar in Partier:
+        plass = Partier.index(svar)
+        Stemmer[plass] = Stemmer[plass] + 1
+        print("Du stemte på:", svar)
+        
+print("Resultat:")
+for i in range(len(Partier)):
+    if Stemmer[i] > 0:
+        print(Partier[i], "fikk", Stemmer[i], "stemmer")
