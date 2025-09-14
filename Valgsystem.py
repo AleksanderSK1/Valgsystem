d31283563d1a2755863ee2ce3ed96fb841597bc5
AleksanderSK1 <alkna009@osloskolen.no>
2025-09-14 22:11:38 +0200

    123

--- Valgsystem.py
+++ Valgsystem.py
@@ -1,24 +1,34 @@
 #Valgsystem
 
-
-Partier = ["arbeiderpartiet", "høyre", "senterpartiet","fremskrittspartiet", "sosialistisk venstreparti", "rødt", "venstre", "miljøpartiet de grønne", "kristelig folkeparti"]
+Partier = ["arbeiderpartiet","høyre","senterpartiet","fremskrittspartiet","sosialistisk venstreparti","rødt","venstre","miljøpartiet de grønne","kristelig folkeparti"]
 Stemmer = [0] * len(Partier)
-print(Partier)
-print("Skriv ferdig hvis du er ferdig med å stemme.")
 
-while True:
-    svar = input("Velg et parti: ")
-    if svar == "ferdig":
-        break
-    if svar in Partier:
-        Stemmer[Partier.index(svar)] += 1
-    else:
-        print("Du stemte på et parti som ikke eksisterer")
+#Viser partiene du kan stemme på
+def vis_partier():
+    print("Disse partiene kan du stemme på:")
+    print(Partier)
 
-total = sum(Stemmer)
+#Funksjon for selve stemmegivningen.
+def stemmegivning():
+    while True: #While løkke som kjøres frem til brukeren sier "ferdig"
+        svar = input("Skriv navnet på partiet du vil stemme på (eller 'ferdig'): ")
+        if svar == "ferdig":
+            break
+        if svar in Partier:
+            plass = Partier.index(svar)
+            Stemmer[plass] = Stemmer[plass] + 1
+        else:
+            print("Det partiet finnes ikke prøv igjen")
+            
+#Funksjonen som lagrer resultatene i en fil
+def lagre_resultat():
+    total = sum(Stemmer)
+    f = open("valgresultat.txt","w")
+    for i in range(len(Partier)):
+        if Stemmer[i] > 0:
+            f.write(Partier[i] + " fikk " + str(Stemmer[i]) + " stemmer (" + str(round(Stemmer[i]/total*100,1)) + "%)\n") #Denne gjør tallene om til prosent
 
-f = open("valgresultat.txt", "w")
-for i in range(len(Partier)):
-    if Stemmer[i] > 0:
-        f.write(Partier[i] + " fikk " + str(Stemmer[i]) + " stemmer (" + str(round(Stemmer[i]/total*100,1)) + "%)\n")
-
+vis_partier()
+print("Skriv ferdig når du er ferdig med å stemme.")
+stemmegivning()
+lagre_resultat()
