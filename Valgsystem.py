8d433ad43c3bb2f8092869adf2cccfd0adeb580d
AleksanderSK1 <alkna009@osloskolen.no>
2025-09-15 11:07:58 +0200

    baller

--- Valgsystem.py
+++ Valgsystem.py
@@ -1,8 +1,7 @@
 #Valgsystem
 
 Partier = ["arbeiderpartiet","høyre","senterpartiet","fremskrittspartiet","sosialistisk venstreparti","rødt","venstre","miljøpartiet de grønne","kristelig folkeparti"]
-Stemmer = [0] * len(Partier)
-
+Stemmer = [0] * len(Partier) # Her har jeg vaglt å bruke len så jeg slipper å lage en liste som dette [0,0,0,0,0,osv]
 #Viser partiene du kan stemme på
 def vis_partier():
     print("Disse partiene kan du stemme på:")
@@ -12,9 +11,9 @@ def vis_partier():
 def stemmegivning():
     while True: #While løkke som kjøres frem til brukeren sier "ferdig"
         svar = input("Skriv navnet på partiet du vil stemme på (eller 'ferdig'): ")
-        if svar == "ferdig":
+        if svar == "ferdig": #If løkke som sier at hvis du skriver ferdig så breaker løkken.
             break
-        if svar in Partier:
+        if svar in Partier: #If løkke som gjør at hvis du skriver et parti feil eller et parti som ikke eksisterer så printer den en beskjed.
             plass = Partier.index(svar)
             Stemmer[plass] = Stemmer[plass] + 1
         else:
@@ -23,7 +22,7 @@ def stemmegivning():
 #Funksjonen som lagrer resultatene i en fil
 def lagre_resultat():
     total = sum(Stemmer)
-    f = open("valgresultat.txt","w")
+    f = open("resultat.txt","w")
     for i in range(len(Partier)):
         if Stemmer[i] > 0:
             f.write(Partier[i] + " fikk " + str(Stemmer[i]) + " stemmer (" + str(round(Stemmer[i]/total*100,1)) + "%)\n") #Denne gjør tallene om til prosent
